ac4400b7365b7708158f2a2050f65397cec73ece
DifaaNug <difanugraha03@gmail.com>
2026-07-14 21:03:18 +0700

    fix: use authorName instead of author in article page

--- app/artikel/[slug]/page.tsx
+++ app/artikel/[slug]/page.tsx
@@ -120,7 +120,7 @@ export default async function ArticlePage({ params }: PageProps) {
                   d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"
                 />
               </svg>
-              {article.author}
+              {article.authorName}
             </span>
           </div>
         </div>
